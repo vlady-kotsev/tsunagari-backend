af0cbe1dfa713234678bdb2e568a8e0ff16913cf
vlady-kotsev <vladislav.kotsev@limechain.tech>
2024-11-16 11:34:18 +0200

    fix: remove unused decorators from dto

--- src/transactions/dto/transaction.dto.ts
+++ src/transactions/dto/transaction.dto.ts
@@ -1,5 +1,3 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
 /**
  * Data Transfer Object for creating a new transaction record.
  * Contains all necessary information for tracking a cross-chain token transfer.
@@ -8,43 +6,31 @@ export class CreateTransactionDto {
   /**
    * The address of the user initiating the transaction.
    */
-  @IsNotEmpty()
-  @IsString()
   user: string;
 
   /**
    * The token contract address on the origin chain.
    */
-  @IsNotEmpty()
-  @IsString()
   originTokenAddress: string;
 
   /**
    * The token contract address on the destination chain.
    */
-  @IsNotEmpty()
-  @IsString()
   destinationTokenAddress: string;
 
   /**
    * The amount of tokens being transferred, as a string to handle large numbers.
    */
-  @IsNotEmpty()
-  @IsString()
   amount: string;
 
   /**
    * The chain ID of the origin network.
    */
-  @IsNotEmpty()
-  @IsNumber()
   originChainId: number;
 
   /**
    * The chain ID of the destination network.
    */
-  @IsNotEmpty()
-  @IsNumber()
   destinationChainId: number;
 }
 
